f14b05f26999af6fb43253d5ed4fd71555c02d24
Lepekhin Nikita <lepekhinnikita@gmail.com>
2023-03-30 23:19:37 +0400

    add TestNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        int amount = 900;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemainBounderMinus() {
+        int amount = 999;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainBounderPlus() {
+        int amount = 1001;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainEquals() {
+        int amount = 1000;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 1000;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+}
