ef5e8bedc93ae88b948743bdf36f1b81c85ece9e
Lepekhin Nikita <lepekhinnikita@gmail.com>
2023-03-30 23:50:36 +0400

    add JUnit4 and tests

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
